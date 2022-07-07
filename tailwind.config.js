de91f3f7f6d630ae06dd3874a89708e830ebc40c
Dansando8 <d.sandoval@vertciel.ca>
2022-07-07 12:39:09 -0400

    boilerplate created

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  darkMode: "class",
+  content: ['./pages/*html'],
+  theme: {
+    extend: {
+      animation: {
+        "fade":"fade 3s ease-out"
+      }, 
+      keyframes: {
+        fade: {
+          "0%, 100%" : {opacity: 1}, 
+          "50%": {opacity: 0}
+        }
+      }
+    },
+  },
+  plugins: [],
+};
